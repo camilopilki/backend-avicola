ed3db4dfe7f6aa1333f4646355c692d86c9583a5
CamiloPilquiman <camilopd92@gmail.com>
2024-07-28 22:39:57 -0400

    arreglos base

--- src/models/db.js
+++ src/models/db.js
@@ -2,11 +2,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  host: process.env.DB_HOST || 'e764qqay0xlsc4cz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: process.env.DB_USER || 'pvnuc41wie039e30',
+  password: process.env.DB_PASSWORD || 'x0ega1yxmk998sv0',
+  database: process.env.DB_NAME || 'qdegpwc8nrfijz1s',
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
